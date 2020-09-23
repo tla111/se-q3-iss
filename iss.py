dac78a435f97d4119010024795653cbae18cac65
Tim La <email@example.com>
2020-09-22 20:36:59 -0400

    tried part A

--- iss.py
+++ iss.py
@@ -2,9 +2,13 @@
 
 __author__ = 'Timothy La (tla111)'
 
+import requests
+
 
 def main():
-    pass
+
+    r = requests.get("http://api.open-notify.org/astros.json")
+    print(r.json())
 
 
 if __name__ == '__main__':
