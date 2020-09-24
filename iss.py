024554663bde5f0f2b37027f5679f2fa60bc9bd9
Tim La <email@example.com>
2020-09-23 22:21:44 -0400

    testing

--- iss.py
+++ iss.py
@@ -7,8 +7,8 @@ import requests
 
 def main():
 
-    r = requests.get("http://api.open-notify.org/astros.json")
-    print(r.json())
+    # r = requests.get("http://api.open-notify.org/astros.json")
+    # print(r.json())
 
 
 if __name__ == '__main__':
