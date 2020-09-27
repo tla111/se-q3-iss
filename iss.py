d0245ad6f2d9f3eab6fd75b4b70c65f9c1e4b4e1
Tim La <email@example.com>
2020-09-27 18:57:32 -0400

    figuring out part c

--- iss.py
+++ iss.py
@@ -3,6 +3,7 @@
 __author__ = 'Timothy La (tla111)'
 
 import requests
+import turtle
 
 
 def main():
@@ -12,7 +13,20 @@ def main():
 
     coords = requests.get(
         'http://api.open-notify.org/iss-now.json').json()
-    return coords
+    print(coords)
+    # return coords
+
+    turtle_screen = turtle.getscreen()
+    turtle_screen.bgcolor("black")
+    # turtle_screen.setworldcoordinates(-51.5177, 114.3406)
+    # turtle_screen.setworldcoordinates(-1, -1, 20, 20)
+    # t = turtle.Turtle()
+    # for i in range(3):
+    #     for colors in ["red", "magenta", "blue", "cyan", "yellow", "white"]:
+    #         t.pen(pencolor=colors, pensize=2, speed=9)
+    #         t.circle(90)
+    #         t.left(10)
+    return turtle_screen
 
 
 if __name__ == '__main__':
